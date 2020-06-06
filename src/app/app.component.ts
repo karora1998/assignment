560b2606fcbe7891dcbca8c708e112a1b0972fa2
karora1998 <karora1_be16@thapar.edu>
2020-06-07 00:45:24 +0530

    export todos button added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { trigger, state, style, transition, animate, keyframes, group } from '@angular/animations';
 
 import { AngularFirestore } from '@angular/fire/firestore';
+
+import * as jsPDF from 'jspdf'
 
 import { ItemsService } from './services/item.service';
 
@@ -36,6 +38,8 @@ export class AppComponent {
 	name: ''
   }
 
+  @ViewChild('list') list: ElementRef;
+
   items: Observable<any[]>;
 
   constructor(private db: AngularFirestore, private itemsService: ItemsService) {}
@@ -49,5 +53,23 @@ export class AppComponent {
   	this.isLoading = true;
   	this.item.name = this.text;
   	this.itemsService.add(this.item);
+  }
+
+  onClickDownload() {
+  	let list=this.list.nativeElement;
+    let doc = new jsPDF();
+    let elementHandlers =
+    {
+      '#editor':function(element,renderer){
+        return true;
+      }
+    };
+    doc.fromHTML(list.innerHTML,15,15,{
+
+      'width':190,
+      'elementHandlers': elementHandlers
+    });
+
+    doc.save('list.pdf');
   }
 }
